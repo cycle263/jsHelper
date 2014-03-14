66805e9637c9d1abdab2a96f760260717380d005
moltboy <cycle263@163.com>
2014-03-14 14:10:31 +0800

    Update branching.js

--- Design/branching.js
+++ Design/branching.js
@@ -1,3 +1,17 @@
 MyNamespace.Singleton = (function(){
-
+	var objectA = {
+		attribute: true,
+		method: function(){
+			
+		}	
+	};
+	
+	var objectB = {
+		attribute: false,
+		method: function(){
+			
+		}	
+	};
+	
+	return (someCondition) ? objectA : objectB;
 })();
