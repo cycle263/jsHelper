5257ee12ceb1677e6a5a29905cedcff8d2466d01
moltboy <cycle263@163.com>
2014-03-14 09:39:05 +0800

    Update singleModel.js

--- Design/singleModel.js
+++ Design/singleModel.js
@@ -59,7 +59,29 @@ NeweggCorp.DataParser = {
 
 //方式二
 NeweggCorp.DataParser = (function(){
-
-});
+	//Private attributes
+	var whitespaceRegex = /\s+/;
+	
+	//Private methods
+	function stripWhitespace(str){
+		return str.replace(whitespaceRegex, '');
+	}
+	
+	function stringSplit(str, delimiter){
+		return str.split(delimiter);
+	}
+	
+	//Everything returned in the object literal is public, but can access the members in the closure created above.
+	return {
+		//Public method
+		stringToArray: function(str, delimiter, stripWS){
+			if(stripWS){
+				str = stripWhitespace(str);
+			}
+			var outputArray = stringSplit(str, delimiter);
+			return outputArray;
+		}
+	};
+})(); //Invoke the function and assign the returned object literal to NeweggCorp.DataParser
 
 addLoadEvent(NeweggCorp.RegPage.init);
