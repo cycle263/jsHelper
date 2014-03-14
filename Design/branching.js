a523537f7c6f5b89d5121f0f0b55739ce5ef80f4
moltboy <cycle263@163.com>
2014-03-14 15:12:28 +0800

    Update branching.js

--- Design/branching.js
+++ Design/branching.js
@@ -15,3 +15,43 @@ MyNamespace.Singleton = (function(){
 	
 	return (someCondition) ? objectA : objectB;
 })();
+
+/////////////////////// SimpleXHRFactory singleton ///////////////////////////
+var simpleXHRFactory = (function(){
+	//The three branches
+	var standard = {
+		createXHRObject: function(){
+			return new XMLHttpRequest();
+		}	
+	};
+	
+	var activeXNew = {
+		createXHRObject: function(){
+			return new ActiveXObject('Msxml2.XMLHTTP');
+		}
+	};
+	
+	var activeXOld = {
+		createXHRObject: function(){
+			return new ActiveXObject('Microsoft.XMLHTTP');
+		}
+	};
+	
+	var testObject;
+	try{
+		testObject = standard.createXHRObject();
+		return standard; //return this if no error was thrown
+	}catch(e){
+		try{
+			testObject = activeXNew.createXHRObject();
+			return activeXNew; //Return this if no error was thrown
+		}catch(e){
+			try{
+				testObject = activeXOld.createXHRObject();
+				return activeXOld; //Return this if no error was thrown
+			}catch(e){
+				throw new Error("No XHR object found in this environment.");
+			}
+		}
+	}
+})();
