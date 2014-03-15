1a074af97e48ed19d7f58368d636594d8fdc7b29
moltboy <cycle263@163.com>
2014-03-15 17:08:35 +0800

    Update XHRfactory.js

--- Design/Model/XHRfactory.js
+++ Design/Model/XHRfactory.js
@@ -1,1 +1,16 @@
 /*  AjaxHandler interface  */
+var AjaxHandler = new Interface('AjaxHandler', ['request', 'createXHRObject']);
+
+/*  SimpleHandler class  */
+var SimpleHandler = function(){};  //implements AjaxHandler
+SimplerHandler.prototype = {
+    request: function(method, url, callback, postVars){
+        var xhr = this.createXHRObject();
+        xhr.onreadystatechange = function(){
+            if(xhr.readyState !== 4) return;
+            (xhr.status === 200) ? callback.success(xhr.responseText, xhr.responseXML) :
+                callback.failure(xhr.status);
+        };
+        xhr.open(method, url, true);
+    }
+}
