9d6ade1f4d1f55585c09e059bdbcf3bd5e6cc942
moltboy <cycle263@163.com>
2014-03-15 14:04:40 +0800

    Update Factory.js

--- Design/Model/Factory.js
+++ Design/Model/Factory.js
@@ -50,4 +50,66 @@ speerster.prototype = {
 
 
 /********************** 真正工厂模式 ************************/
-/*  BicycleShop class */
+/*  BicycleShop class(abstract) */
+var BicycleShop = function(){
+    //...
+};
+BicycleShop.prototype = {
+    sellBicycle: function(model){
+        var bicycle = this.createBicycle(model);
+        bicycle.assemble();
+        bicycle.wash();
+        return bicycle;
+    },
+    createBicycle: function(model){
+        throw new Error('Unsupported operation on an abstract class.');
+    }
+};
+
+/*  GanitBicycleShop class  */
+var GanitBicycleShop = function(){};
+extend(GanitBicycleShop, BicycleShop);
+GanitBicycleShop.prototype.createBicycle = function(model){
+    var bicycle;
+    
+    switch(model){
+        case 'The Speedster':
+            bicycle = new GanitSpeedster();
+            break;
+        case 'The Lowrider':
+            bicycle = new GanitLowrider();
+            break;
+        case 'The Flatlander':
+            bicycle = new GanitFlatlander();
+            break;
+        case 'The Comfort Cruiser':
+        default:
+            bicycle = new GanitComfortCruiser();
+    }
+    Interface.ensureImplements(bicycle, Bicycle);
+    return bicycle;
+};
+
+/*  GeneralBicycleShop class  */
+var GeneralBicycleShop = function(){};
+extend(GeneralBicycleShop, BicycleShop);
+GeneralBicycleShop.prototype.createBicycle = function(model){
+    var bicycle;
+    
+    switch(model){
+        case 'The Speedster':
+            bicycle = new GeneralSpeedster();
+            break;
+        case 'The Lowrider':
+            bicycle = new GeneralLowrider();
+            break;
+        case 'The Flatlander':
+            bicycle = new GeneralFlatlander();
+            break;
+        case 'The Comfort Cruiser':
+        default:
+            bicycle = new GeneralComfortCruiser();
+    }
+    Interface.ensureImplements(bicycle, Bicycle);
+    return bicycle;
+};
