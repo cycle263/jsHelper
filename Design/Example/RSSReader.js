8aa4cc6f9e5c9c62df159d33b012b90c7abda99e
moltboy <cycle263@163.com>
2014-03-18 16:35:49 +0800

    Update RSSReader.js

--- Design/Example/RSSReader.js
+++ Design/Example/RSSReader.js
@@ -1,1 +1,9 @@
-/**/
+/*  DisplayModule interface  */
+var DisplayModule = new Interface('DisplayModule', ['append', 'remove', 'clear']);
+
+/*  ListDisplay class  */
+var ListDisplay = function(id, parent){  //implements DisplayModule
+    this.list = document.createElement('ul'); 
+    this.list.id = id;
+    parent.appendChild(this.list);
+}
