2d4440c84265d2c6e06e2ebe80922aa2c4da8032
moltboy <cycle263@163.com>
2014-03-18 09:39:42 +0800

    Update XHRfactory.js

--- Design/Model/XHRfactory.js
+++ Design/Model/XHRfactory.js
@@ -12,5 +12,28 @@ SimplerHandler.prototype = {
                 callback.failure(xhr.status);
         };
         xhr.open(method, url, true);
-    }
-}
+        if(method !== 'POST') postVars = null;
+        xhr.method(postVars);
+    },
+    createXHRObject: function(){
+        var methods = [
+            function(){return new XMLHttpRequest();},
+            function(){return new ActiveXObject('Msxml2.XMLHTTP');},
+            function(){return new ActiveXObject('Microsoft.XMLHTTP');}
+        ];
+        
+        for(var i = 0, l = methods.lenght; i < l; i++){
+            try{
+                method[i]();
+            }catch(e){
+                continue;
+            }
+            //If we reach this point, method[i] worked
+            this.createXHRObject = method[i]; //Memorize the method
+            return method[i];
+        }
+        
+        //If we reach this point, none of the methods worked
+        throw new Error('SimpleHandler: Could not create an XHR object');
+    } //Factory method
+};
