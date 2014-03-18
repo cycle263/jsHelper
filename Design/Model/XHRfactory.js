7e4bf10cd22fdd6f605f5e1c19898bebaf8b4b50
moltboy <cycle263@163.com>
2014-03-18 14:14:28 +0800

    Update XHRfactory.js

--- Design/Model/XHRfactory.js
+++ Design/Model/XHRfactory.js
@@ -114,3 +114,37 @@ OfflineHandler.prototype.flushStoreRequests = function(){
         OfflineHandler.super.request(req.method, req.url, req.callback, req.postVars);
     }
 };
+
+/* XHRManager singleton */
+var XHRManager = {
+    createXHRHandler: function(){
+        var xhr;
+        if(this.isOffline()){
+            xhr = new OfflineHandler();
+        }else if(this.isHighLatency()){
+            xhr = new QueuedHandler();
+        }else{
+            xhr = new SimpleHandler();
+        }
+        
+        Interface.ensureImplements(xhr, AjaxHandler);
+        return xhr;
+    },
+    isOffline: function(){
+        //Do a quick request with SimpleHandler and see if it succeeds
+    },
+    isHighLatency: function(){
+        //Do a series of requests with SimpleHandler and time the responses. Best done once, as a branching function
+    }
+};
+
+var myHandler = XHRManager.createXHRHandler();
+var callback = {
+    success: function(resText){
+        console.log('Success: ' + resText);
+    },
+    failure: function(statusCode){
+        console.log('Failure: ' + statusCode);
+    }
+};
+myHandler.request('GET', 'script.php', callback);
