e8435f536a7db345db605f0780f212271644f756
moltboy <cycle263@163.com>
2014-10-24 17:11:15 +0800

    Create 鲜为人知的javascript.js

---
+++ Other/鲜为人知的javascript.js
@@ -0,0 +1,28 @@
+/* 获取伪元素属性 */
+// Get the color value of .element:before
+var color = window.getComputedStyle(
+	document.querySelector('.element'), ':before'
+).getPropertyValue('color');
+
+// Get the content value of .element:before
+var content = window.getComputedStyle(
+	document.querySelector('.element'), ':before'
+).getPropertyValue('content');
+
+/* 原生javascript修改样式class类 */
+myDiv.classList.add('myCssClass'); // Adds a class
+myDiv.classList.remove('myCssClass'); // Removes a class
+myDiv.classList.toggle('myCssClass'); // Toggles a class
+
+/* 添加样式规则 */
+function addCSSRule(sheet, selector, rules, index) {
+	if(sheet.insertRule) {
+		sheet.insertRule(selector + "{" + rules + "}", index);
+	}
+	else {
+		sheet.addRule(selector, rules, index);
+	}
+}
+
+// Use it!
+addCSSRule(document.styleSheets[0], "header", "float: left");
