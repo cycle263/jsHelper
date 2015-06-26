e3f01b3419c835317d0324270b7e931c96996382
moltboy <cycle263@163.com>
2015-06-26 16:43:24 +0800

    Update molt-helper-utils.js

--- molt-helper-utils.js
+++ molt-helper-utils.js
@@ -139,14 +139,7 @@ function getElementsByClassName(node, name){
 	return array;
 }
 
-//计算对象的长度
-Object.size = function(obj) {
-    var size = 0, key;
-    for (key in obj) {
-        if (obj.hasOwnProperty(key)) size++;
-    }
-    return size;
-};
+
 
 
 /***************** 比对外链样式表名称 *******************/
@@ -185,18 +178,6 @@ app.directive('verifyinput', function(){
 });
 //组合键中keyCode值与单独的keyCode有一定出入
 
-
-/*******知数组中某个对象的属性值，删除数组中的该对象******/
-function delObjArr(arrs,k,v，o){     //o为是否进行删除的开关
-    o ? o : false;
-    for(var i = 0, l = arrs.length; i < l; i++){
-        if(arrs[i][k] == v){
-            console.log('index: ' + i + ', value: ' + arrs[i][k]);
-            o && arrs.splice(i, 1);
-            return i；
-        }
-    }
-}
 
 //计算某字段属性在数组中的index
     function IndexOfObject(arr, filed, value) {
@@ -540,110 +521,6 @@ target.fire({"type":"message", "message": "event message - very fine!"});
 
 target.removeHandler("message", messageHandler);
 target.fire({"type":"message", "message": "event message - very fine!"});
-
-
-
-/*****************   克隆函数   ****************/
-function clone(object){
-	function F(){}
-	F.prototype = object;
-	return new F;
-}
-function clone(obj){
-	var target = {};
-	for(var i in obj){
-		obj.hasOwnProperty(i) && (target[i] = obj[i]);
-	}
-	return target;
-}
-var cloneObje = Object.clone(oldObj);
-var cloneObj = JSON.parse(JSON.stringify(oldObj));
-
-//jQuery clone function
-var jQcloneObj = function(){
-	var target = arguments[0] || {}, i = 1, len = arguments.length, 
-		deep = false, options, name, src, copy,
-		toString = Object.prototype.toString;
-
-	if(typeof target === 'boolean'){
-		deep = target;
-		target = arguments[1] || {};
-		i = 2;
-	}
-
-	//Object.prototype.toString.call(target).slice(8, -1) !== 'Function'
-	if(typeof target !== 'object' && toString.call(target) !== '[object Function]'){
-		target = {};
-	}
-
-	// Handle case when target is a string or something (possible in deep copy)
-	if(len === i){
-		target = this;
-		--i;
-	}
-
-	for(; i < len; i++){
-		// Only deal with non-null/undefined values
-		if((options = arguments[i]) != null){
-			for(name in options){
-				src = target[name];
-				copy = options[name];
-
-				// Prevent never-ending loop
-				if(target === copy){continue;}
-
-				// Recurse if we're merging object literal values or arrays
-				if(deep && copy && (toString.call(copy) !== 'Object' || toString.call(copy) !== 'Array')){
-					var clone = src && (toString.call(src) !== 'Object' || toString.call(src) !== 'Array') ? src :
-							(toString.call(copy) !== 'Array'	? [] : {});
-
-					// Never move original objects, clone them
-					target[name] = jQcloneObj(deep, clone, copy);
-				}else if(copy !== undefined){
-					target[name] = copy;
-				}
-			}
-		}
-	}
-	return target;
-};
-
-///简易的克隆对象
-function cloneSimapleObject(obj){
-	var temp = JSON.stringify(obj);
-	return JSON.parse(temp);
-}
-
-//////////////////   序列化对象   ///////////////////
-function serialize(obj){
-    var output = [], toString = Object.prototype.toString;
-
-    for(var key in obj){
-        if(toString.call(obj[key]).slice(8, -1) === 'Object' || toString.call(obj[key]).slice(8, -1) === 'Array'){
-            output.push(serialize(obj[key]));
-        }else{
-        	if(toString.call(obj[key]).slice(8, -1) === 'Function'){
-        		obj.hasOwnProperty(key) && output.push(key + ': ' + obj[key]);
-        	}else{
-        		obj.hasOwnProperty(key) && output.push(key + '": "' + obj[key]);
-        	}            
-        }                    
-    }
-
-    return output.join('", "');
-}
-
-function augment(receivingClass, givingClass){
-	if(arguments[2]){
-		for(var i = 2, len = arguments.length; i++){
-			receivingClass.prototype[arguments[i]] || (receivingClass.prototype[arguments[i]] = givingClass.prototype[arguments[i]]);
-		}
-	}else{
-		for(methodName in givingClass.prototype){
-			receivingClass.prototype[methodName] || (receivingClass.prototype[methodName] = givingClass.prototype[methodName]);
-		}
-	}
-}
 
 
 ///////////////////////  就地编辑域  //////////////////////////
